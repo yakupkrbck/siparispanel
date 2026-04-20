e8b524f96d89cd161bf64391ab8a1dad863110e3
yakupkrbck <yakupkrbck@gmail.com>
2026-04-21 02:14:44 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.cirolog.ai',
+  appName: 'CiroLog AI',
+  webDir: 'dist'
+};
+
+export default config;
